5d5d1de196ade5b558d04eb102f48f9714657bf1
Haihua Li <haihua.sysu@gmail.com>
2019-02-07 01:56:31 -0800

    Add parseFromString proto test

--- clockSync/src/test_proto.cc
+++ clockSync/src/test_proto.cc
@@ -5,12 +5,19 @@ using namespace std;
 
 int main() {
     NetworkRequest request;
-    request.set_client_id(1);
+    request.set_client_id(123);
     request.set_client_ip("127.0.0.1");
     request.set_type(NetworkRequest::CLOCK_SYNC);
 
+    // serialize to string
     string *coding = new string();
     request.SerializeToString(coding);
-    cout << (*coding).length() << endl;
+
+    // parse from string
+    NetworkRequest newRequest;
+    newRequest.ParseFromString(*coding);
+    cout << newRequest.client_id() << endl;
+    cout << newRequest.client_ip() << endl;
+    cout << newRequest.type() << endl;
     return 0;
 }
